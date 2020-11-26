9c071e3fc935f8f215bb8dd1bd4918dbbf136987
oldghoast <2281901186@qq.com>
2020-11-26 10:01:06 +0800

    2020.11.26

--- src/main/java/com/spring/HelloServlet.java
+++ src/main/java/com/spring/HelloServlet.java
@@ -3,8 +3,6 @@ package com.spring;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,12 +15,12 @@ public class HelloServlet extends HttpServlet {
 
     @Override
     public void init() throws ServletException {
-        System.out.println("初始化Servlet...");
+        log.error("初始化Servlet...");
     }
 
     @Override
     public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        System.out.println("是我执行了doGet方法，我才是入口");
+        log.error("是我执行了doGet方法，我才是入口");
         doGet(req, res);
     }
 
@@ -35,6 +33,6 @@ public class HelloServlet extends HttpServlet {
 
     @Override
     public void destroy() {
-        System.out.println("Destroy...");
+        log.error("Destroy...");
     }
 }
--- src/main/java/com/spring/controller/DispatcherServlet.java
+++ src/main/java/com/spring/controller/DispatcherServlet.java
@@ -1,6 +1,7 @@
 package com.spring.controller;
 
 import com.spring.controller.frontend.MainPageController;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,11 +15,14 @@ import java.io.IOException;
  * 2.解析请求
  * 3.派发给对应的Controller里面的方法进行处理
  */
+@Slf4j
 @WebServlet("/")
 public class DispatcherServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        if (req.getServletPath() == "/frontend/getmainpageinfo" && req.getMethod() == "GET") {
+        log.debug("拦截所有请求");
+        if ("/frontend/getmainpageinfo".equals(req.getServletPath()) && "GET".equals(req.getMethod())) {
+            log.debug("/frontend/getmainpageinfo请求");
             new MainPageController().getMainPageInfo(req, resp);
         }
     }
